c7af37258559d2b712b3236cc55be58a51d1a6ff
tuyojr <olutuyod@gmail.com>
2023-03-17 07:32:17 +0100

    required providers

---
+++ jenkins-pipeline/k8s/terraform.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.48.0"
+    }
+
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.16.1"
+    }
+
+    acme = {
+      source  = "vancluever/acme"
+      version = "~> 2.0"
+    }
+
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0.4"
+    }
+
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.0.1"
+    }
+  }
+}
