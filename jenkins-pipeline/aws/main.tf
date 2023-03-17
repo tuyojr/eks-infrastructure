b3468c8f612b02f96c859ec8a77fe7e9186b4dba
tuyojr <olutuyod@gmail.com>
2023-03-17 14:20:32 +0100

    provision infrastructure

--- jenkins-pipeline/aws/main.tf
+++ jenkins-pipeline/aws/main.tf
@@ -5,13 +5,7 @@ provider "aws" {
   region = var.region
 }
 
-data "terraform_remote_state" "jenkins-server" {
-  backend = "local"
-
-  config = {
-    path = "../../jenkins-server/terraform.tfstate"
-  }
-}
+data "aws_availability_zones" "available" {}
 
 locals {
   cluster_name = "tuyojr-eks-${random_string.suffix.result}"
@@ -22,6 +16,37 @@ resource "random_string" "suffix" {
   special = false
 }
 
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "3.19.0"
+
+  name = "tuyojr-vpc"
+
+  cidr = "10.0.0.0/16"
+  azs  = slice(data.aws_availability_zones.available.names, 0, 3)
+
+  private_subnets = ["10.0.1.0/24", "10.0.3.0/24", "10.0.5.0/24"]
+  public_subnets  = ["10.0.0.0/24", "10.0.2.0/24", "10.0.4.0/24"]
+
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+  }
+
+  public_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/elb"                      = 1
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/internal-elb"             = 1
+  }
+}
+
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "19.5.1"
@@ -29,8 +54,8 @@ module "eks" {
   cluster_name    = local.cluster_name
   cluster_version = "1.25"
 
-  vpc_id                         = data.terraform_remote_state.jenkins-server.outputs.aws_vpc.value
-  subnet_ids                     = data.terraform_remote_state.jenkins-server.outputs.aws_subnet.value
+  vpc_id                         = module.vpc.vpc_id
+  subnet_ids                     = module.vpc.private_subnets
   cluster_endpoint_public_access = true
 
   eks_managed_node_group_defaults = {
