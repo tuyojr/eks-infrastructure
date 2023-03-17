7dbc409184e82c1d07a2942d1af58ab14a18faa3
tuyojr <olutuyod@gmail.com>
2023-03-17 08:07:12 +0100

    updated subnet

--- jenkins-server/jenkins-server.tf
+++ jenkins-server/jenkins-server.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "jenkins-server" {
     ami = "ami-0557a15b87f6559cf"
     instance_type = "t2.micro"
     key_name = "awsKeyPair"
-    subnet_id = module.vpc.public_subnets[0]
+    subnet_id = module.vpc.private_subnets[0]
     vpc_security_group_ids = [aws_security_group.jenkins-sg.id]
     availability_zone = module.vpc.azs[0]
     associate_public_ip_address = true
