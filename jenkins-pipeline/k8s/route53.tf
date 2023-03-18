daec871ad2396af2f72d690d83cc2566dce6c501
tuyojr <olutuyod@gmail.com>
2023-03-18 22:42:10 +0100

    updated record sets

--- jenkins-pipeline/k8s/route53.tf
+++ jenkins-pipeline/k8s/route53.tf
@@ -14,6 +14,7 @@ resource "aws_route53_record" "sub-domain-1" {
   zone_id = aws_route53_zone.domain.zone_id
   name    = local.defaults[1]
   type    = "A"
+  ttl     = "300" 
 
   records = ["a58001c47e5694136b099ac5511110c9-185002891.us-east-1.elb.amazonaws.com"]
 }
@@ -22,6 +23,7 @@ resource "aws_route53_record" "sub-domain-2" {
   zone_id = aws_route53_zone.domain.zone_id
   name    = local.defaults[2]
   type    = "A"
+  ttl     = "300"
 
   records = ["a8f4bbcf797914a53adb1a0c8838140a-502033186.us-east-1.elb.amazonaws.com"]
 }
