80171b39e08fc0fdf82035d4919656397877f891
tuyojr <olutuyod@gmail.com>
2023-03-18 10:47:30 +0100

    updated configuration files

--- jenkins-pipeline/aws/main.tf
+++ jenkins-pipeline/aws/main.tf
@@ -16,17 +16,14 @@ resource "random_string" "suffix" {
   special = false
 }
 
-module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "3.19.0"
-
-  name = "tuyojr-vpc"
-
-  cidr = "10.0.0.0/16"
-  azs  = slice(data.aws_availability_zones.available.names, 0, 3)
-
-  private_subnets = ["10.0.1.0/24", "10.0.3.0/24", "10.0.5.0/24"]
+module "tuyojr-vpc" {
+  source          = "terraform-aws-modules/vpc/aws"
+  version         = "3.19.0"
+  name            = "tuyojr-vpc"
+  cidr            = "10.0.0.0/16"
   public_subnets  = ["10.0.0.0/24", "10.0.2.0/24", "10.0.4.0/24"]
+  private_subnets = ["10.0.1.0/24", "10.0.3.0/24", "10.0.5.0/24"]
+  azs             = slice(data.aws_availability_zones.available.names, 0, 3)
 
   enable_nat_gateway   = true
   single_nat_gateway   = true
@@ -38,32 +35,31 @@ module "vpc" {
 
   public_subnet_tags = {
     "kubernetes.io/cluster/${local.cluster_name}" = "shared"
-    "kubernetes.io/role/elb"                      = 1
+    "kubernetes.io/role/elb"                  = 1
   }
 
   private_subnet_tags = {
     "kubernetes.io/cluster/${local.cluster_name}" = "shared"
-    "kubernetes.io/role/internal-elb"             = 1
+    "kubernetes.io/role/internal-elb"         = 1
   }
 }
 
 module "eks" {
-  source  = "terraform-aws-modules/eks/aws"
-  version = "19.5.1"
+    source  = "terraform-aws-modules/eks/aws"
+    version = "~> 19.0"
+    cluster_name = "${local.cluster_name}"
+    cluster_version = "1.24"
 
-  cluster_name    = local.cluster_name
-  cluster_version = "1.25"
+    cluster_endpoint_public_access  = true
 
-  vpc_id                         = module.vpc.vpc_id
-  subnet_ids                     = module.vpc.private_subnets
-  cluster_endpoint_public_access = true
+    vpc_id = module.tuyojr-vpc.vpc_id
+    subnet_ids = module.tuyojr-vpc.private_subnets
 
-  eks_managed_node_group_defaults = {
-    ami_type = "AL2_x86_64"
+    tags = {
+      
+    }
 
-  }
-
-  eks_managed_node_groups = {
+    eks_managed_node_groups = {
     one = {
       name = "worker-node-1"
 
