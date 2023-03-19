8e1b123a628521c1e885952c55a995ebe0ac59b1
tuyojr <olutuyod@gmail.com>
2023-03-19 12:23:48 +0100

    updated eks version

--- jenkins-pipeline/aws/main.tf
+++ jenkins-pipeline/aws/main.tf
@@ -48,7 +48,7 @@ module "eks" {
     source  = "terraform-aws-modules/eks/aws"
     version = "~> 19.0"
     cluster_name = "${local.cluster_name}"
-    cluster_version = "1.24"
+    cluster_version = "1.25"
 
     cluster_endpoint_public_access  = true
 
