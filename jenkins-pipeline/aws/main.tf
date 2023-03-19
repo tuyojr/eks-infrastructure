f27610fb837d8c2188e6a25b82ef30777eb2c70b
tuyojr <olutuyod@gmail.com>
2023-03-19 09:28:39 +0100

    updated pipeline

--- jenkins-pipeline/aws/main.tf
+++ jenkins-pipeline/aws/main.tf
@@ -95,12 +95,10 @@ module "eks" {
 provider "kubernetes" {
   host                   = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-  kubeconfig {
-    exec {
-      api_version = "client.authentication.k8s.io/v1beta1"
-      args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
-      command     = "aws"
-    }
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
+    command     = "aws"
   }
 }
 
