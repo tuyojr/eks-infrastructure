53eeafdb517d0ffde2085d9de949a31327989cd1
tuyojr <olutuyod@gmail.com>
2023-03-19 12:14:16 +0100

    updated ami and script

--- jenkins-server/jenkins-server.tf
+++ jenkins-server/jenkins-server.tf
@@ -1,12 +1,12 @@
 resource "aws_instance" "jenkins-server" {
-    ami = "ami-005f9685cb30f234b"
+    ami = "ami-09cd747c78a9add63"
     instance_type = var.instance_type
     key_name = "awsKeyPair"
     subnet_id = aws_subnet.public_subnets_1.id
     vpc_security_group_ids = [aws_security_group.jenkins-sg.id]
     availability_zone = aws_subnet.public_subnets_1.availability_zone
     associate_public_ip_address = true
-    user_data = file("script.sh")
+    user_data = file("ubuntu-script.sh")
     tags = {
         Name = "jenkins-server"
     }
